cadb0dfe31de7125ff9098a97c7fdc6667a979ed
Hovhannes Petrosyan <Hovopetrosyan7@gmail.com>
2021-06-10 15:05:30 +0400

    pch

--- js/script.js
+++ js/script.js
@@ -1,7 +1,32 @@
-// sarqel pole chudes
+let sum = document.getElementById("sum");
+let gum = 5;
+sum.textContent = gum ;
+for (let i = 97; i < 123; i++) {
+    const button = document.createElement("button");
+    button.classList.add("btn");
+    button.textContent = String.fromCharCode(i);
+    button.value = String.fromCharCode(i);
 
-// min 8 tar 
+    const keyb = document.getElementsByClassName("keyb")[0];
+    keyb.appendChild(button);
 
-// sxalvelu 5 hnaravorutyun
+    const pat = document.getElementsByClassName("tar");
 
-// verev@ bar@ , tak@ tarer@
+    for (let k = 0; k < pat.length; k++) {  
+        button.addEventListener("click", function(event){
+           if(event.target.textContent == pat[k].textContent){
+              pat[k].classList.add("tarjs");
+              event.target.style.backgroundColor = "blue";
+              event.target.style.color = "white";
+            }else if (event.target.textContent !== pat[k].textContent) {
+                // gum -= 1;
+                // sum.textContent = gum;
+            }
+            event.target.disabled = true;
+        })
+    }
+} 
+
+function myFunc() {
+    location.reload()
+}
